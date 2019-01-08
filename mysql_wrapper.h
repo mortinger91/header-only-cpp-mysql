c53a7077dd7336dfe49fc01ada5ce5f99f255069
mortinger91 <micheleemiliani91@gmail.com>
2019-01-08 16:58:48 +0100

    Update mysql_wrapper.h

--- mysql_wrapper.h
+++ mysql_wrapper.h
@@ -36,7 +36,7 @@ bool mysql_connect(const char *host_name, const char *user_name, const char *pas
 // close connection to the server
 void mysql_disconnect()
 {
-	mysql_close (conn);
+    mysql_close (conn);
 }
 
 // print in the stdout the result of a SELECT query and the number
@@ -44,84 +44,83 @@ void mysql_disconnect()
 // modify the code inside the while for custom behaviour
 bool mysql_print_select(std::string selectQuery_string)
 {
-	const char* selectQuery = &selectQuery_string[0];
-	if (mysql_query (conn, selectQuery) != 0)
-	{
-		std::cout << "SELECT query failed" << std::endl;
-		return false;
-	}
+    const char* selectQuery = &selectQuery_string[0];
+    if (mysql_query (conn, selectQuery) != 0)
+    {
+        std::cout << "SELECT query failed" << std::endl;
+        return false;
+    }
 
-	// generate the result set
-	MYSQL_RES *res_set;
-	res_set = mysql_store_result (conn);      
-	if (res_set == NULL)
-	{
-		std::cout << "mysql_store_result() failed" << std::endl;
-		return false;
-	}
+    // generate the result set
+    MYSQL_RES *res_set;
+    res_set = mysql_store_result (conn);      
+    if (res_set == NULL)
+    {
+        std::cout << "mysql_store_result() failed" << std::endl;
+        return false;
+    }
 
-	// processing for each row
-	MYSQL_ROW row;
-	while ((row = mysql_fetch_row (res_set)) != NULL)
-	{
-		for (unsigned int i = 0; i < mysql_num_fields (res_set); i++)
-		{
-			if (i > 0)
-			fputc ('\t', stdout);
-			printf ("%s", row[i] != NULL ? row[i] : "NULL");
-		}
-		fputc ('\n', stdout);
-	}
+    // processing for each row
+    MYSQL_ROW row;
+    while ((row = mysql_fetch_row (res_set)) != NULL)
+    {
+        for (unsigned int i = 0; i < mysql_num_fields (res_set); i++)
+        {
+            if (i > 0)
+            fputc ('\t', stdout);
+            printf ("%s", row[i] != NULL ? row[i] : "NULL");
+        }
+        fputc ('\n', stdout);
+    }
 
 	// error checking
 	if (mysql_errno (conn) != 0)
 	{
-		std::cout << "mysql_fetch_row() failed" << std::endl;
+        std::cout << "mysql_fetch_row() failed" << std::endl;
 	}
 	else
 	{
-		std::cout << (unsigned long) mysql_num_rows (res_set) 
-			<< " rows returned" << std::endl;
+        std::cout << (unsigned long) mysql_num_rows (res_set) 
+            << " rows returned" << std::endl;
 	}
 
 	// freeing the memory allocated for the result set
 	mysql_free_result (res_set);
-
 	return true;
 }
 
 // function for a INSERT query
 bool mysql_insert(std::string insertQuery_string)
 {
-	const char* insertQuery = &insertQuery_string[0];
-	if (mysql_query (conn, insertQuery) != 0)
-	{
-      std::cout << "INSERT statement failed" << std::endl;
-      return false;
-	}
-	std::cout << "INSERT statement succeeded: " << (unsigned long) mysql_affected_rows (conn) 
-		<< " rows affected" << std::endl;
-	return true;
+    const char* insertQuery = &insertQuery_string[0];
+    if (mysql_query (conn, insertQuery) != 0)
+    {
+        std::cout << "INSERT statement failed" << std::endl;
+        return false;
+    }
+    std::cout << "INSERT statement succeeded: " << (unsigned long) mysql_affected_rows (conn) 
+        << " rows affected" << std::endl;
+    return true;
 }
 
 // example function which connects and perform a select and an insert query
 void example(const char *host_name, const char *user_name, const char *password, std::string selectQuery, std::string insertQuery)
 {
-	if (!mysql_connect(host_name, user_name, password))
-	{
-		std::cout << "error on connect" << std::endl;
-		return;
-	}
-	if (!mysql_print_select(selectQuery))
-	{
-		std::cout << "error on select" << std::endl;
-		return;
-	}
-	if (!mysql_insert(insertQuery))
-	{
-		std::cout << "error on insert" << std::endl;
-		return;
-	}
-	std::cout << "all operations completed" << std::endl;
-	return;
+    if (!mysql_connect(host_name, user_name, password))
+    {
+        std::cout << "error on connect" << std::endl;
+        return;
+    }
+    if (!mysql_print_select(selectQuery))
+    {
+        std::cout << "error on select" << std::endl;
+        return;
+    }
+    if (!mysql_insert(insertQuery))
+    {
+        std::cout << "error on insert" << std::endl;
+        return;
+    }
+    std::cout << "all operations completed" << std::endl;
+    return;
 }
